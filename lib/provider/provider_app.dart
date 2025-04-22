b0bfe30517a3b31d78909c8eef2fe41ac6341b5c
AlexMajumder <alexmajumder55@gmail.com>
2025-04-22 21:49:48 +0600

    add provider

---
+++ lib/provider/provider_app.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+class ProviderApp extends StatelessWidget {
+  const ProviderApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
